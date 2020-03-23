4791be2cfddd177ac6f9f91e1bee2eab84b200eb
Kosh <koshatul@users.noreply.github.com>
2020-03-23 10:09:03 +1000

    removed leading v from versions for crtsh-ls and goembed

--- Formula/crtsh-ls.rb
+++ Formula/crtsh-ls.rb
@@ -2,7 +2,7 @@ class CrtshLs < Formula
   desc "query the crt.sh certificate transparency log from the CLI."
   homepage "https://github.com/koshatul/crtsh-ls"
 
-  version "v0.3.0"
+  version "0.3.0"
   url "https://github.com/koshatul/crtsh-ls/releases/download/v0.3.0/crtsh-ls-v0.3.0-darwin-amd64.zip"
   sha256 "f215a93fb5702a57eff2e7fb5871cf8e6612583e0a9f8ec6512eeeab51515839"
 
--- Formula/goembed.rb
+++ Formula/goembed.rb
@@ -2,7 +2,7 @@ class Goembed < Formula
   desc "goembed takes a list of local files and embeds them into golang source files."
   homepage "https://github.com/koshatul/goembed"
 
-  version "v0.4.0"
+  version "0.4.0"
   url "https://github.com/koshatul/goembed/releases/download/v0.4.0/goembed-v0.4.0-darwin-amd64.zip"
   sha256 "e31e3af5fc39d80dbdd2596cf4059da5452312e053f41a1f2515189df55db0e1"
 
